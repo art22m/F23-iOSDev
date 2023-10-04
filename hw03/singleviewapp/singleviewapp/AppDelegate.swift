f5c520921fbb4b15b313d5d7aa7f0a23144a5c49
Artem Murashko <art22m@yandex-team.ru>
2023-10-04 20:47:51 +0300

    initial commit

---
+++ hw03/singleviewapp/singleviewapp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  singleviewapp
+//
+//  Created by Artem Murashko on 04.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow()
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+
+        self.window = window
+        
+        return true
+    }
+
+}
+
