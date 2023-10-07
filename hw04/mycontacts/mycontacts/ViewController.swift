c94304db499433ec8fccf6f28c71687139a24df7
Artem Murashko <art22m@yandex-team.ru>
2023-10-07 17:38:33 +0300

    initial commit

---
+++ hw04/mycontacts/mycontacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mycontacts
+//
+//  Created by Artem Murashko on 07.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
