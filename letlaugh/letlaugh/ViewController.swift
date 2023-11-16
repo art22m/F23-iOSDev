7c523c13c8475777a4b2b359f624bcc707e97ac1
Artem Murashko <art22m@yandex-team.ru>
2023-11-16 16:04:59 +0300

    init project

---
+++ letlaugh/letlaugh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  letlaugh
+//
+//  Created by Artem Murashko on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
